4e0fe94bda7fd64d7e88b6156430c71d23881c5c
Shreeya <shreeyasharmma@gmail.com>
2024-08-12 18:23:07 +0530

    implemented all crud endpoints

--- To_Do_Project/app/urls.py
+++ To_Do_Project/app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("getAllData/", views.getAllData, name="getAllDone"),
     path("getStatusData/", views.getStatusData, name="getStatusData"),
     path("addTask/", views.addTask, name="addTask"),
+    path("deleteTask/", views.deleteTask, name="deleteTask"),
+    path("modifyStatus/", views.modifyStatus, name="modifyStatus"),
 ]
--- To_Do_Project/app/views.py
+++ To_Do_Project/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import UserTasks
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def getAllData(request):
     username = request.GET.get('username', None)
@@ -20,16 +22,38 @@ def getStatusData(request):
     }
     return JsonResponse(JSONData)
 
-
+@csrf_exempt
 def addTask(request):
     if(request.method == "POST"):
-        print(request.body)
+        data = json.loads(request.body)
+        username = data.get('username', None)
+        taskname = data.get('taskname', None)
+        status = data.get('status', None)
+        newEntry = UserTasks(username=username, taskname=taskname, status=status)
+        newEntry.save()
     return HttpResponse("Added")
 
+@csrf_exempt
 def deleteTask(request):
-    pass
+    if(request.method == "POST"):
+        data = json.loads(request.body)
+        username = data.get('username', None)
+        taskname = data.get('taskname', None)
+        status = data.get('status', None)
+        UserTasks.objects.filter(username=username, taskname=taskname, status=status).delete()
+    return HttpResponse("Deleted")    
 
-def modifyTask(request):
-    pass
+@csrf_exempt
+def modifyStatus(request):
+    if(request.method == "POST"):
+        data = json.loads(request.body)
+        username = data.get('username', None)
+        taskname = data.get('taskname', None)
+        status = data.get('status', None)
+        task = UserTasks.objects.get(username=username, taskname=taskname)
+        if(status is not None):
+            task.status = status
+        task.save()          
+    return HttpResponse("Status changed")
 
 
